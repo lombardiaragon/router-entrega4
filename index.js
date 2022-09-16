287e338ac896b00198c852ee4f9e4f942ec092af
Federico Lombardi <federicolombardi_10@hotmail.com>
2022-09-15 21:48:34 -0300

    e

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const express=require('express')
+const handlebars=require('express-handlebars')
 const app=express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-const routerProductos=require('./productos')
 
-app.use('/api/productos', routerProductos)
+const Productos=[]
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/views'))
+
+app.engine('hbs', handlebars.engine({
+    extname:'hbs',
+    layoutDir:__dirname+'./views/layouts',
+    defaultLayout:'index'
+}))
+
+app.set('views','./views')
+app.set('view engine', 'hbs')
+
+//* En la ruta raíz me trae el formulario
+app.get('/',(req,res)=>{            
+    res.render('main',{layout:'index'})
+
+})
+
+//* En la ruta /productos me renderiza la lista
+app.get('/productos',(req,res)=>{            
+    res.render('main', {layout:'productos',productos:Productos})
+
+})
+
+//guardado de los productos cargados
+app.post('/productos', (req,res)=>{
+    const newProd= req.body
+    newProd.id=Productos.length +1
+    Productos.push(newProd)
+    res.redirect('/')
+})
+
 
 
 app.listen(8080,()=>{
